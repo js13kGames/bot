07176b8f83cd93e5c25cf91b42748a06b7c512d4
platane <me@platane.me>
2019-07-17 22:08:28 +0200

    feat: read / set comment in the PR to add the report

---
+++ src/comment.ts
@@ -0,0 +1,72 @@
+import { GithubClient } from "./services/github";
+import { PullRequest } from "./types/github";
+import * as config from "./config";
+import { parseReport, Bundle, generateReport } from "./report";
+
+/**
+ * read the comment left by the bot
+ */
+export const getComment = ({ github }: { github: GithubClient }) => async (
+  pullRequest: PullRequest
+) => {
+  const { data: comments } = await github.issues.listComments({
+    owner: pullRequest.base.repo.owner.login,
+    repo: pullRequest.base.repo.name,
+    number: pullRequest.number,
+    per_page: 100
+  });
+
+  const userLogin = config.github.app_name + "[bot]";
+
+  console.log(comments.map(({ user }) => user.login));
+
+  const comment = comments.find(({ user }) => user.login === userLogin);
+
+  return comment;
+};
+
+/**
+ * set the comment
+ * we want the bot to update the comment instead of creating new owes
+ * to avoid spam
+ */
+export const setComment = ({ github }: { github: GithubClient }) => async (
+  pullRequest: PullRequest,
+  body: string
+) => {
+  const comment = await getComment({ github })(pullRequest);
+
+  if (comment) {
+    await github.issues.updateComment({
+      owner: pullRequest.base.repo.owner.login,
+      repo: pullRequest.base.repo.name,
+      comment_id: comment.id,
+      body
+    });
+  } else
+    await github.issues.createComment({
+      owner: pullRequest.base.repo.owner.login,
+      repo: pullRequest.base.repo.name,
+      number: pullRequest.number,
+      body
+    });
+};
+
+/**
+ * read the comment and extract the report
+ */
+export const getReport = ({ github }: { github: GithubClient }) => async (
+  pullRequest: PullRequest
+): Promise<Bundle[]> => {
+  const comment = await getComment({ github })(pullRequest);
+
+  return comment ? parseReport(comment.body) : [];
+};
+
+/**
+ * format the report and set the comment
+ */
+export const setReport = ({ github }: { github: GithubClient }) => async (
+  pullRequest: PullRequest,
+  bundles: Bundle[]
+) => setComment({ github })(pullRequest, generateReport(bundles));
