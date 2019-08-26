e4f2a6df063c6db3b32370465e4b92414f8adf9b
platane <me@platane.me>
2019-08-26 11:45:53 +0200

    feat: disable submit check run

--- src/report/checkRuns/index.ts
+++ src/report/checkRuns/index.ts
@@ -1,7 +1,6 @@
 import { generateBundleCheckRun } from "./generateBundleCheckRun";
 import { generateMetaCheckRun } from "./generateMetaCheckRun";
 import { generateGameCheckRun } from "./generateGameCheckRun";
-import { generateSubmitCheckRun } from "./generateSubmitCheckRun";
 import { Control } from "../../analyze/control";
 import { CheckRun } from "../../services/github";
 
@@ -9,7 +8,5 @@ export const generateCheckRuns = (controls: Control[]): CheckRun[] =>
   [
     generateMetaCheckRun(controls),
     generateBundleCheckRun(controls),
-    generateGameCheckRun(controls),
-    controls.every(s => s.conclusion !== "failure") &&
-      generateSubmitCheckRun(controls)
+    generateGameCheckRun(controls)
   ].filter(Boolean);
