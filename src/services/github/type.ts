1827517a4b1fc6942c78b0b25aa401aa744ceca3
platane <me@platane.me>
2019-09-12 10:26:30 +0200

    feat: bump octokit rest ; drop unmaintened github-app

--- src/services/github/type.ts
+++ src/services/github/type.ts
@@ -1,7 +1,5 @@
-import createApp from "github-app";
-import * as config from "../config";
 import Github, {
-  PullRequestsListResponseItem,
+  PullsListResponseItem,
   ReposListAssetsForReleaseResponseItem,
   ReposGetReleaseResponse,
   ChecksGetResponse,
@@ -9,18 +7,18 @@ import Github, {
   AppsGetAuthenticatedResponse,
   ChecksUpdateParams,
   ChecksGetSuiteResponse,
-  PullRequestsListFilesResponseItem,
+  PullsListFilesResponseItem,
   ReposGetCommitResponse
 } from "@octokit/rest";
 
 export type Commit = ReposGetCommitResponse;
-export type PullRequest = PullRequestsListResponseItem;
+export type PullRequest = PullsListResponseItem;
 export type Repository = PullRequest["head"]["repo"];
 export type Asset = ReposListAssetsForReleaseResponseItem;
 export type Release = ReposGetReleaseResponse;
 export type Installation = AppsGetInstallationResponse;
 export type App = AppsGetAuthenticatedResponse;
-export type File = PullRequestsListFilesResponseItem;
+export type File = PullsListFilesResponseItem;
 export type CheckSuite = Omit<ChecksGetSuiteResponse, "pull_requests"> & {
   pull_requests: PullRequest[];
 };
@@ -29,34 +27,6 @@ export type CheckRun = Omit<ChecksGetResponse, "conclusion"> & {
 };
 
 export type GithubClient = Github;
-
-export const create = (installationId: number): GithubClient =>
-  createApp({
-    id: config.github.app_id,
-    cert: config.github.app_private_key
-  }).asInstallation(installationId);
-
-export const listInstallations = async (): Promise<Installation[]> => {
-  const github = await createApp({
-    id: config.github.app_id,
-    cert: config.github.app_private_key
-  }).asApp();
-
-  const { data: installations } = await github.apps.listInstallations({});
-
-  return installations;
-};
-
-export const getApp = async (): Promise<App> => {
-  const github = await createApp({
-    id: config.github.app_id,
-    cert: config.github.app_private_key
-  }).asApp();
-
-  const { data: app } = await github.apps.getAuthenticated({});
-
-  return app;
-};
 
 type GenericEventPayload = {
   repository: Repository;
@@ -76,6 +46,11 @@ export type Event = (
   | {
       eventName: "pull_request";
       action: "opened";
+      pull_request: PullRequest;
+    }
+  | {
+      eventName: "pull_request";
+      action: "reopened";
       pull_request: PullRequest;
     }
   | {
