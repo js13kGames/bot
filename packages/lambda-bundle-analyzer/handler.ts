f464ad28a8115b1d722afa56db11cb039ca8b284
platane <me@platane.me>
2022-07-31 13:51:52 +0200

    ♻️

--- packages/lambda-bundle-analyzer/handler.ts
+++ packages/lambda-bundle-analyzer/handler.ts
@@ -7,31 +7,9 @@ import {
 } from "js13kGames-bot-rules";
 import { wrapHandle } from "./wrapHandle";
 
-// const handle_ = async (event: APIGatewayProxyEvent) => {
-//   console.log("coucou", JSON.stringify(process.versions));
-
-//   return { status: "ok", ...process.versions };
-
-//   // const body = await parseBody(event);
-
-//   // console.log(JSON.stringify(body));
-
-//   // const bundle: File = body.bundle as any;
-
-//   // const categories = (
-//   //   Array.isArray(body.category) ? body.category : [body.category]
-//   // ).filter(isValidCategory);
-
-//   // const rules = mergeRules(...categories.map(getRulesForCategory));
-
-//   // if (!rules) return { categories, rules, checks: [] };
-
-//   // const report = await analyze(rules, bundle.content);
-
-//   // return { categories, rules, ...formatReport(report) };
-// };
-
 export const handle = wrapHandle(async (event) => {
+  console.log(JSON.stringify(event));
+
   const body = await parseBody(event);
 
   const bundle: File = body.bundle as any;
